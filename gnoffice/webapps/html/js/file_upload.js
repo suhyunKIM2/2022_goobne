3dc5ce0e7d5005c6201a1737ff7e544525161e80
suhyunKIM2 <87849293+suhyunKIM2@users.noreply.github.com>
2022-04-20 17:14:02 +0900

    게시판 퍼블리싱 진행중

---
+++ gnoffice/webapps/html/js/file_upload.js
@@ -0,0 +1,26 @@
+﻿console.log()
+function handleFileSelect(event) {
+    var input = this;
+    console.log(input.files)
+    if (input.files && input.files.length) {
+        var reader = new FileReader();
+        this.enabled = false
+        reader.onload = (function (e) {
+        console.log(e)
+            $("#preview").html(['<img class="thumb" src="', e.target.result, '" title="', escape(e.name), '"/>'].join(''));
+            $("#preview").css('display','block');
+            $(".file-edit-icon").css('display','block');
+        });
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+$('#file').change(handleFileSelect);
+$('.file-edit-icon').on('click', '.preview-de', function () {
+    $("#preview").css('display','none');
+    $(".file-edit-icon").css('display','none');
+    $("#preview").empty()
+    $("#file").val("");
+});
+$('.preview-edit').click( function() {
+  $("#file").click();
+} );
