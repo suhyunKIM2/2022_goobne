8f6cf5c09ef6aaa73656fee77b5e006b98726303
suhyunKIM2 <87849293+suhyunKIM2@users.noreply.github.com>
2022-06-17 11:46:05 +0900

    관리자 배너 관련 수정 완료

--- gnorder/webapps/html/assets/js/main/main.js
+++ gnorder/webapps/html/assets/js/main/main.js
@@ -139,7 +139,7 @@ $(document).ready(function () {
         slidesToScroll: 1, //스크롤 한번에 움직일 컨텐츠 개수
         arrows: false, // 옆으로 이동하는 화살표 표시 여부
         dots: true, // 스크롤바 아래 점으로 페이지네이션 여부
-        autoplay: false, // 자동 스크롤 사용 여부
+        autoplay: true, // 자동 스크롤 사용 여부
         autoplaySpeed: 2000, // 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
        
     });
